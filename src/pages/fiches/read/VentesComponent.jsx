7b47793de2f271a1e64239330cdd5abec16749bd
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-06-25 12:46:42 +0200

    Deal with empty ventes

--- src/pages/fiches/read/VentesComponent.jsx
+++ src/pages/fiches/read/VentesComponent.jsx
@@ -26,49 +26,60 @@ const VentesComponent = ({ ventes, deleteVente, postVente }) => {
             <Table.HeaderCell textAlign='center'></Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-        <Table.Body>
-          {ventes.map(
-            ({
-              id,
-              id_marche,
-              unite,
-              rendement_min,
-              rendement,
-              rendement_max,
-              mois_relatif,
-              mois,
-            }) => {
-              return (
-                <Table.Row key={id}>
-                  <Table.Cell>{id_marche} Nom du marché Nom produit</Table.Cell>
-                  <Table.Cell textAlign='center'>
-                    {rendement_min} {rendement_min && unite}
-                  </Table.Cell>
-                  <Table.Cell textAlign='center'>
-                    {rendement} {rendement && unite}
-                  </Table.Cell>
-                  <Table.Cell textAlign='center'>
-                    {rendement_max} {rendement_max && unite}
-                  </Table.Cell>
-                  <Table.Cell textAlign='center'>
-                    {mois_relatif} {mois && getMois(mois)}
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Button
-                      onClick={() => deleteVente(id)}
-                      size='mini'
-                      icon
-                      basic
-                      circular
-                    >
-                      <Icon name='trash' />
-                    </Button>
-                  </Table.Cell>
-                </Table.Row>
-              );
-            }
-          )}
-        </Table.Body>
+
+        {ventes.length === 0 ? (
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Aucune vente pour le moment...</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        ) : (
+          <Table.Body>
+            {ventes.map(
+              ({
+                id,
+                id_marche,
+                unite,
+                rendement_min,
+                rendement,
+                rendement_max,
+                mois_relatif,
+                mois,
+              }) => {
+                return (
+                  <Table.Row key={id}>
+                    <Table.Cell>
+                      {id_marche} Nom du marché Nom produit
+                    </Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      {rendement_min} {rendement_min && unite}
+                    </Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      {rendement} {rendement && unite}
+                    </Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      {rendement_max} {rendement_max && unite}
+                    </Table.Cell>
+                    <Table.Cell textAlign='center'>
+                      {mois_relatif} {mois && getMois(mois)}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        onClick={() => deleteVente(id)}
+                        size='mini'
+                        icon
+                        basic
+                        circular
+                      >
+                        <Icon name='trash' />
+                      </Button>
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              }
+            )}
+          </Table.Body>
+        )}
       </Table>
       {isFormOpen ? (
         <Button onClick={() => setIsFormOpen(false)}>Annuler</Button>
