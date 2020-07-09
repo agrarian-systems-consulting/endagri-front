2c8ce5dea87151ad8afe46dadace389b2d1e014d
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-07-09 10:40:13 +0200

    List Fiches OK

--- src/pages/fiches/list/ListFichesPage.jsx
+++ src/pages/fiches/list/ListFichesPage.jsx
@@ -1,44 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Breadcrumb, Table, Button } from 'semantic-ui-react';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Grid, Breadcrumb, Table, Button, Icon } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
+import Axios from 'axios';
 
 const ListFichesPage = () => {
-  const [fiches, setFiches] = useState([
-    {
-      id: 1,
-      id_production: 24,
-      production: 'Carottes',
-      libelle: 'Carottes en sols sableux',
-      id_utilisateur: 63,
-      utilisateur: 'Asma Turki',
-      ini_debut: 3,
-      ini_fin: 5,
-      modified: '2021-07-10',
-      created: '2021-07-05',
-      commentaire: 'Un commentaire sur cette première fiche',
-    },
-    {
-      id: 2,
-      id_production: 24,
-      production: 'Carottes',
-      libelle: 'Carottes en sols argileux',
-      id_utilisateur: 63,
-      utilisateur: 'Asma Turki',
-      ini_debut: 3,
-      ini_fin: 5,
-      modified: '2021-07-08',
-      created: '2021-07-07',
-      commentaire: 'Un commentaire sur cette seconde fiche',
-    },
-  ]);
+  const [fiches, setFiches] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function getFiches() {
-      // axios.get
-      // const data = await MyAPI.getAnalyses();
-      // setFiches(data)
-    }
-    getFiches();
+    const fetchData = async () => {
+      const res = await Axios.get(`http://localhost:3333/fiches`);
+      setFiches(res.data);
+      setLoading(false);
+    };
+    fetchData();
   }, []);
 
   return (
@@ -50,44 +25,68 @@ const ListFichesPage = () => {
           </Breadcrumb>
         </Grid.Column>
       </Grid.Row>
-      <Grid.Row>
-        <Grid.Column width={16}>
-          <Table singleLine>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell></Table.HeaderCell>
-                <Table.HeaderCell>Production</Table.HeaderCell>
-                <Table.HeaderCell>Libellé</Table.HeaderCell>
-                <Table.HeaderCell>Date de création</Table.HeaderCell>
-                <Table.HeaderCell>Dernière modification</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-          
-              {fiches.map(({ id, production, libelle, created, modified }) => {
-                return (
-                  <Table.Row key={id}>
-                    <Table.Cell>
-                      <NavLink to={`/fiche/${id}`}>Fiche {id}</NavLink>
-                    </Table.Cell>
-                    <Table.Cell>{production}</Table.Cell>
-                    <Table.Cell>{libelle}</Table.Cell>
-                    <Table.Cell>{created}</Table.Cell>
-                    <Table.Cell>{modified}</Table.Cell>
+      {loading ? (
+        <p>Chargement en cours...</p>
+      ) : (
+        <Fragment>
+          <Grid.Row>
+            <Grid.Column width={16}>
+              <Table singleLine>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell></Table.HeaderCell>
+                    <Table.HeaderCell>Libellé</Table.HeaderCell>
+                    <Table.HeaderCell>Production</Table.HeaderCell>
+                    <Table.HeaderCell></Table.HeaderCell>
                   </Table.Row>
-                );
-              })}
-            </Table.Body>
-          </Table>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column width={16}>
-          <Button color='teal' as={NavLink} to='/fiche/create'>
-            Nouvelle fiche
-          </Button>
-        </Grid.Column>
-      </Grid.Row>
+                </Table.Header>
+                <Table.Body>
+                  {fiches.map(
+                    ({
+                      id,
+                      libelle_production,
+                      libelle,
+                      created,
+                      modified,
+                    }) => {
+                      return (
+                        <Table.Row key={id}>
+                          <Table.Cell>
+                            <NavLink to={`/fiche/${id}`}>Fiche {id}</NavLink>
+                          </Table.Cell>
+                          <Table.Cell>
+                            <NavLink to={`/fiche/${id}`}>{libelle}</NavLink>
+                          </Table.Cell>
+                          <Table.Cell>{libelle_production}</Table.Cell>
+                          <Table.Cell>
+                            <Button
+                              size='mini'
+                              icon
+                              basic
+                              circular
+                              as={NavLink}
+                              to={`/fiche/${id}/delete`}
+                            >
+                              <Icon name='trash' />
+                            </Button>
+                          </Table.Cell>
+                        </Table.Row>
+                      );
+                    }
+                  )}
+                </Table.Body>
+              </Table>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16}>
+              <Button color='teal' as={NavLink} to='/fiche/create'>
+                Nouvelle fiche
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Fragment>
+      )}
     </Grid>
   );
 };
