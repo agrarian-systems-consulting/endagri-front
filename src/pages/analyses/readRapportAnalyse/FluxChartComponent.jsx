f1aff7ac3adb7458279d79af468dda48e5d26c1e
Hugo Lehoux <45370179+agrarian-systems@users.noreply.github.com>
2020-08-05 10:18:42 +0200

    Add Chart

---
+++ src/pages/analyses/readRapportAnalyse/FluxChartComponent.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+  Line,
+  ComposedChart,
+} from 'recharts';
+
+const FluxChartComponent = ({ data }) => {
+  const chartData = data;
+  data.map((d) => (d.total_depenses = d.total_depenses * -1));
+
+  return (
+    <ResponsiveContainer width='100%' height={400}>
+      <ComposedChart
+        barCategoryGap={10}
+        data={chartData}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='mois' />
+        <YAxis
+          yAxisId='left'
+          label={{
+            value: 'Ventes et Dépenses',
+            angle: -90,
+            position: 'insideLeft',
+          }}
+        />
+        <YAxis
+          yAxisId='right'
+          orientation='right'
+          label={{
+            value: 'Trésorerie',
+            angle: 90,
+            position: 'insideRight',
+          }}
+        />
+        <Tooltip />
+        <Legend />
+
+        <Bar
+          yAxisId='left'
+          dataKey='total_depenses'
+          fill='#F47560'
+          name='Dépenses'
+        />
+        <Bar
+          yAxisId='left'
+          dataKey='total_ventes'
+          fill='#F1E059'
+          name='Ventes'
+        />
+        <Line
+          yAxisId='right'
+          ype='monotone'
+          dataKey='solde_cumule'
+          stroke='#97E3D4'
+          strokeWidth={2}
+          name='Solde cumulé'
+        />
+      </ComposedChart>
+      {/* <pre>{JSON.stringify(chartData, true, 2)}</pre> */}
+    </ResponsiveContainer>
+  );
+};
+
+export default FluxChartComponent;
--- src/pages/analyses/readRapportAnalyse/ReadFluxMoisReelsParFichesLibres.jsx
+++ src/pages/analyses/readRapportAnalyse/ReadFluxMoisReelsParFichesLibres.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Segment, Table } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
 import Axios from 'axios';
+import FluxChartComponent from './FluxChartComponent';
 
 const ReadFluxMoisReelsParFichesLibres = () => {
   let { id } = useParams();
@@ -17,37 +18,45 @@ const ReadFluxMoisReelsParFichesLibres = () => {
   }, []);
 
   return (
-    <Segment>
-      <h5>Résumé</h5>
-      Période d'analyse : Remettre ici Trésorerie initiale : 1500
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Mois</Table.HeaderCell>
-            <Table.HeaderCell>Dépenses</Table.HeaderCell>
-            <Table.HeaderCell>Ventes</Table.HeaderCell>
-            <Table.HeaderCell>Solde</Table.HeaderCell>
-            <Table.HeaderCell>Trésorerie</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {flux.map(
-            ({ mois, total_depenses, total_ventes, solde, solde_cumule }) => {
-              return (
-                <Table.Row>
-                  <Table.Cell>{mois}</Table.Cell>
-                  <Table.Cell>{total_depenses}</Table.Cell>
-                  <Table.Cell>{total_ventes}</Table.Cell>
-                  <Table.Cell>{solde}</Table.Cell>
-                  <Table.Cell>{solde_cumule}</Table.Cell>
-                </Table.Row>
-              );
-            }
-          )}
-        </Table.Body>
-      </Table>
-      {/* <pre>{JSON.stringify(flux, true, 2)}</pre> */}
-    </Segment>
+    <Fragment>
+      <Segment>
+        <h5>Flux de trésorerie</h5>
+        <FluxChartComponent data={flux} />
+      </Segment>
+      <Segment>
+        <h5>Résumé</h5>
+        <h4>Période d'analyse </h4> Récupérer la bonne valeur ici
+        <h4>Trésorerie initiale</h4> Récupérer la bonne valeur ici (exemple
+        analyse 1 = 1500 TND)
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Mois</Table.HeaderCell>
+              <Table.HeaderCell>Dépenses</Table.HeaderCell>
+              <Table.HeaderCell>Ventes</Table.HeaderCell>
+              <Table.HeaderCell>Solde</Table.HeaderCell>
+              <Table.HeaderCell>Trésorerie</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {flux.map(
+              ({ mois, total_depenses, total_ventes, solde, solde_cumule }) => {
+                return (
+                  <Table.Row>
+                    <Table.Cell>{mois}</Table.Cell>
+                    <Table.Cell>{total_depenses}</Table.Cell>
+                    <Table.Cell>{total_ventes}</Table.Cell>
+                    <Table.Cell>{solde}</Table.Cell>
+                    <Table.Cell>{solde_cumule}</Table.Cell>
+                  </Table.Row>
+                );
+              }
+            )}
+          </Table.Body>
+        </Table>
+        <pre>{JSON.stringify(flux, true, 2)}</pre>
+      </Segment>
+    </Fragment>
   );
 };
 
